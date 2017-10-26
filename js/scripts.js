c9cf4dd7258b308f0c4b89911245e937f724991e
Connor Miller and Mikey Pinaud <connor@>
2017-10-26 14:50:10 -0700

    it will add a for loop to apply a symbol to a number

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
-$(function(){
-  $("form").submit(function(event){
-    event.preventDefault();
-    var userInput = $("input").val();
-    $("#result").text(userInput).val();
+    var romanSymbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    var numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    var result = " ";
+    var replaceNum = function(_userInput) {
+      for (var index = 0; index < numbers.length; index += 1) {
+        while (numbers[index] <= _userInput) {
+          result += romanSymbols[index];
+          _userInput -= numbers[index];
 
+        }
+      }
+      return result;
+    }
+
+$(function(){
+  $("form").click(function(event){
+    var userInput = $("#numOnly").val();
+    var romanNumeral = replaceNum(userInput);
+    $("#result").text(replaceNum);
+
+    event.preventDefault();
   });
 });
